a0e44745ef61268d0abc1aed5a3ac115eac71b84
Your Name <lukasjonaitisltu@gmail.com>
2024-02-19 15:23:48 +0200

    USER_ACC

--- tasker_lukas/tasker_lukas/urls.py
+++ tasker_lukas/tasker_lukas/urls.py
@@ -21,6 +21,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('tasks.urls')),
+    path('user_profiles/', include('user_profiles.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
 ]
--- tasker_lukas/user_profiles/apps.py
+++ tasker_lukas/user_profiles/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class UserProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_profiles'
+    verbose_name = ('user profiles')
