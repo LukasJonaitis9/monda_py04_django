2c557948bac0c4b576c5af74de489ebd31bf4c85
Your Name <lukasjonaitisltu@gmail.com>
2024-02-19 10:06:23 +0200

    User_profile

---
+++ tasker_lukas/user_profiles/views.py
@@ -0,0 +1,8 @@
+from django.contrib import messages
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.utils.translation import gettext_lazy as _
+from . import forms
+
+
+# Create your views here.
